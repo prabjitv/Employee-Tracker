56acf58d0cab678664d890abe072de41ca8d4a3f
prab <58503954+prabjitv@users.noreply.github.com>
2020-05-03 15:04:35 -0700

    seeds and schema delineated

--- schema.sql
+++ schema.sql
@@ -1,83 +1,27 @@
 DROP DATABASE IF EXISTS empTrackDB;
 CREATE DATABASE empTrackDB;
-
 USE empTrackDB;
 
-CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    deptName VARCHAR(45) NULL,
-    PRIMARY KEY (id)
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    deptName VARCHAR(45) NULL
 );
 
-CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(45) NULL,
-  salary DECIMAL NULL,
-  PRIMARY KEY (id),
+  salary DECIMAL NOT NULL,
+  dept_id INT,
+  FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
-  FOREIGN KEY(artistId) REFERENCES artists(id)
-
-
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(45) NULL,
   lastName VARCHAR(45) NULL,
-  
-  PRIMARY KEY (id),
-  
-);
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  manager_id INT,
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+  );
 
--- roleId
---   managerId
--- FOREIGN KEY(artistId) REFERENCES artists(id)
-
-INSERT INTO department (deptName)
-VALUES ("band");
-INSERT INTO department (deptName)
-VALUES ("mgmt");
-INSERT INTO department (deptName)
-VALUES ("record label");
-
-INSERT INTO role (title, salary)
--- VALUES ("musician", "1");
-
--- INSERT INTO role (title, salary)
--- VALUES ("booker", "10");
-
--- INSERT INTO role (title, salary)
--- VALUES ("agent", "10");
-
--- INSERT INTO role (title, salary)
--- VALUES ("musician", "1");
-
-
-
--- INSERT INTO artists (firstName, lastName)
--- VALUES ("Britney", "Spears");
-
--- INSERT INTO artists (firstName, lastName)
--- VALUES ("Billie", "Eilish");
-
-
--- INSERT INTO songs (title, artistId, genre)
--- VALUES ("Wheare Are U Now", 1, "Dance");
-
--- INSERT INTO songs (title, artistId, genre)
--- VALUES ("I Don't Care", 1, "Dance");
-
--- INSERT INTO songs (title, artistId, genre)
--- VALUES ("Toxic",  2, "Dance");
-
--- INSERT INTO songs (title, artistId, genre)
--- VALUES ("No Time to Die", 3, "Soundtrack");
-
--- SELECT * FROM artists;
-
--- SELECT * FROM songs;
-
--- SELECT * FROM songs 
--- INNER JOIN artists on songs.artistId = artists.id;
-
--- SELECT songs.title, songs.genre, artists.firstName, artists.lastName FROM songs 
--- INNER JOIN artists on songs.artistId = artists.id;
---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (deptName)
+VALUES ("Officers"), ("Dept Heads"), ("Staff");
+
+INSERT INTO roles (title, salary)
+VALUES ("Engineer", "1"), ("Engineering Dept Head", "2"), ("CTO", "10");
+
+INSERT INTO employees (firstName, lastName)
+VALUES ("Tom", "Slo"), ("Prab", "Vir"), ("Mike", "Barn");
